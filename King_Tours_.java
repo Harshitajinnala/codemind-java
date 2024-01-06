0fe9584abe900a43e6b0647037d583090fa047ef
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 19:58:41 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     int c = 7*b + a*5;     System.out.printf("%d",c); } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = 7*b + a*5;
+    System.out.printf("%d",c);
+}
+} 
