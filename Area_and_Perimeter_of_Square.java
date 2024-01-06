ad53139f78d386d2a635904f0765b4688740e63d
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 20:12:02 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = a * a;     int c = 4 * a;     System.out.printf("%d ",b);     System.out.printf("%d",c); } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = a * a;
+    int c = 4 * a;
+    System.out.printf("%d ",b);
+    System.out.printf("%d",c);
+}
+} 
