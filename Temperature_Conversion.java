a74e9de4829c7f6e6d04e4f729b6d45443204f67
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 21:03:37 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     double c = sc.nextInt();     double f = 32+(c *9/5);     System.out.printf("%.2f",f); } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double c = sc.nextInt();
+    double f = 32+(c *9/5);
+    System.out.printf("%.2f",f);
+}
+} 
