746b27cb8de804c3aef367cf5a6ea1213f4ef69f
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 20:46:25 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     int c = sc.nextInt();     int d = 3 * a - b - c;     System.out.printf("%d",d); } }

---
+++ Average_weight.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int d = 3 * a - b - c;
+    System.out.printf("%d",d);
+}
+} 
