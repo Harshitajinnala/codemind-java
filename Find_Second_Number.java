8131945939aea523c7151885393463b0887284ae
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 11:22:11 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int c = sc.nextInt();     int b = sc.nextInt();     int a = 2 * c - b;     System.out.printf("%d",a); } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int c = sc.nextInt();
+    int b = sc.nextInt();
+    int a = 2 * c - b;
+    System.out.printf("%d",a);
+}
+} 
