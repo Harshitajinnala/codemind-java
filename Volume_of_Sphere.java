10858f45e06a0cc664d37ef650fe3aa21f0030a8
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-06 10:17:38 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     double c= ((4.0/3.0)*(3.14)*(a*a*a));     System.out.printf("%.2f",c); } }

--- Volume_of_Sphere.java
+++ Volume_of_Sphere.java
@@ -3,8 +3,7 @@ public class MyClass{
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    double pie=3.14;
-    double c= ((4.0/3.0)*(pie)*(a*a*a));
+    double c= ((4.0/3.0)*(3.14)*(a*a*a));
     System.out.printf("%.2f",c);
 }
 } 
