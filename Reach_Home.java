08ac84509db2ae5d11b81c55c86307b6c2b54e70
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-08 21:52:26 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     if((a*5)>=b)     System.out.printf("YES");     else System.out.printf("NO"); } }

---
+++ Reach_Home.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if((a*5)>=b)
+    System.out.printf("YES");
+    else System.out.printf("NO");
+}
+} 
