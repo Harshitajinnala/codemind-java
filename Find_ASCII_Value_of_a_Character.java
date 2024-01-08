d6c7f0d344dc98abcc52d24bd6c17b459cc54b7b
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-08 16:28:51 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     char s = sc.next().charAt(0);     int a = s;     System.out.printf("%d",a); } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    char s = sc.next().charAt(0);
+    int a = s;
+    System.out.printf("%d",a);
+}
+} 
