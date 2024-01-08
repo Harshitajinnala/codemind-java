656137c714e4b8a261efff6136a6afc607364f63
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-08 21:54:34 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     if(a>=(b*2))     System.out.printf("YES");     else System.out.printf("NO"); } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if(a>=(b*2))
+    System.out.printf("YES");
+    else System.out.printf("NO");
+}
+} 
