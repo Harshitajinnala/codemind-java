b20076efec9a9f944030a8ca916e324ecad29b90
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-08 11:24:00 +0530

    import java.util.*;     public class OctalToDecimalExample1{     public static void main(String args[]){     Scanner sc=new Scanner(System.in);     String octal=sc.next();     int decimal=Integer.parseInt(octal,8);     System.out.println(decimal); }}

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+    public class OctalToDecimalExample1{  
+    public static void main(String args[]){  
+    Scanner sc=new Scanner(System.in);  
+    String octal=sc.next();  
+    int decimal=Integer.parseInt(octal,8);  
+    System.out.println(decimal);  
+}}  
