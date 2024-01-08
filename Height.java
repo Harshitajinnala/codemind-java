9ef4489c02cfbf791d66af286aab9d55a6a22a1c
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-08 21:50:17 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     if(a>b)     System.out.printf("%d",a);     else System.out.printf("%d",b); } }

---
+++ Height.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if(a>b)
+    System.out.printf("%d",a);
+    else System.out.printf("%d",b);
+}
+} 
