9f73b683a024d98714e3631604d084f86b11ff5b
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-11 09:53:01 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     if(a>=18)     System.out.printf("YES");     else     System.out.printf("NO"); } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    if(a>=18)
+    System.out.printf("YES");
+    else
+    System.out.printf("NO");
+}
+} 
