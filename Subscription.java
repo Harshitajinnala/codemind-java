c230e1fff22d6fe5ebe645261b94fcd791b91e68
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-12 09:36:38 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     double a = sc.nextDouble();     if(a>40)     System.out.printf("YES");     else     System.out.printf("NO"); } }

---
+++ Subscription.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double a = sc.nextDouble();
+    if(a>40)
+    System.out.printf("YES");
+    else
+    System.out.printf("NO");
+}
+} 
