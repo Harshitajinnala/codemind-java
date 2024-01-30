ea20105715a05c114b192fa5235a084de995063e
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-01-30 11:53:42 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a = sc.nextInt();     int b = sc.nextInt();     int z = sc.nextInt();     int c = (10*b) +(5*a);     int d = c/z;     System.out.printf("%d",d); } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int z = sc.nextInt();
+    int c = (10*b) +(5*a);
+    int d = c/z;
+    System.out.printf("%d",d);
+}
+} 
