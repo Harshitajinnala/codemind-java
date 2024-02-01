4f310634f48846bc46d630089f5900f96d0429f6
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-02-01 11:35:59 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a= sc.nextInt();     int b= sc.nextInt();     for(int i =a;i<=b;i++){
         System.out.printf("%d ",i);
        }
       
    }
    }

---
+++ A_to_B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int b= sc.nextInt();
+    for(int i =a;i<=b;i++){
+    
+     System.out.printf("%d ",i);
+    }
+   
+}
+} 
