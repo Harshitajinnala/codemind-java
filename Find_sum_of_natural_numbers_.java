cc495515bbb1c9700ce7481b69169bbe48e4c107
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-02-01 11:44:44 +0530

    //hexagonal to binary import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a= sc.nextInt();     int n =(a*(a+1))/2;     System.out.println(n); } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+//hexagonal to binary
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int n =(a*(a+1))/2;
+    System.out.println(n);
+}
+} 
