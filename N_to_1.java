5a009cb9e86a50af346cc3a5f7df8ec90b5a2f6e
Jinnala Harshita <119839384+Harshitajinnala@users.noreply.github.com>
2024-02-01 11:30:23 +0530

    //hexagonal to binary import java.util.*; public class MyClass{     public static void main(String[] args){     Scanner sc = new Scanner(System.in);     int a= sc.nextInt();     for(int i =a;i>0;i--){      System.out.printf("%d ",i);     }
    }
    }

---
+++ N_to_1.java
@@ -0,0 +1,12 @@
+//hexagonal to binary
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    for(int i =a;i>0;i--){
+     System.out.printf("%d ",i);
+    }
+   
+}
+} 
